a3e0f33b08c204c67b9a790bd3dbe255c9380be6
hendriwhyu <hendriwp123@gmail.com>
2023-09-17 16:21:52 +0700

    firstCommit

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\LoginToken;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function showLogin()
+    {
+        return view('auth.login');
+    }
+    public function login(Request $request)
+    {
+        $data = $request->validate([
+            'email' => ['required', 'email', 'exists:users,email'],
+        ]);
+        User::whereEmail($data['email'])->first()->sendLoginLink();
+        session()->flash('success', true);
+        return redirect()->back();
+    }
+    public function verifyLogin(Request $request, $token)
+    {
+        $token = LoginToken::whereToken(hash('sha256', $token))->firstOrFail();
+        abort_unless($request->hasValidSignature() && $token->isValid(), 401);
+        $token->consume();
+        Auth::login($token->user);
+        return redirect('/');
+    }
+    public function logout()
+    {
+        Auth::logout();
+        return redirect(route('login'));
+    }
+}
---
+++ app/Models/User.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Models;
+
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use App\Mail\MagicLoginLink;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Mail;
+use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Support\Str;
+
+class User extends Authenticatable
+{
+    use HasApiTokens, HasFactory, Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'email',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+        'password' => 'hashed',
+    ];
+
+    public function sendLoginLink()
+    {
+        $plaintext = Str::random(32);
+        $token = $this->loginTokens()->create([
+                'token' => hash('sha256', $plaintext),
+                'expires_at' => now()->addMinutes(15),
+            ]);
+        Mail::to($this->email)->queue(new MagicLoginLink($plaintext, $token->expires_at));
+    }
+
+    public function loginTokens()
+    {
+        return $this->hasMany(LoginToken::class);
+    }
+}
